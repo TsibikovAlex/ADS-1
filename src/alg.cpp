954a88234b5e8194678ce9217a0b3120ee8258b1
TsibikovAlex <78901893+TsibikovAlex@users.noreply.github.com>
2021-03-29 10:02:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,8 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value)
-{
+int cbinsearch(int *arr, int size, int value) {
   int count = 0;
-  for (int i = 0; i < size; i++)
-  {
+  for (int i = 0; i < size; i++) {
     if (arr[i] == value)
       count++;
   }
